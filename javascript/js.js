188c01d8d731fced1a6a079d06bf7e9f68f28e54
cchristinah <christinathaiha@gmail.com>
2025-02-28 15:26:46 -0500

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,126 @@
+// $("#block1").click(function(){
+//     $("#block1").css("opacity", "1");
+// })
+
+//  if($("#block1").click(function(){
+//         $("#block1").css("opacity", "1")
+//     }
+//     else{
+//         $("#block1").css("opacity", "0.5")
+//     }))
+
+
+
+$(document).ready(function() {
+    $("#block1").click(function() {
+        $(this).toggleClass("active");
+
+        if ($(this).hasClass("active")) {
+            $(this).css("opacity", "1");
+        } else {
+            $(this).css("opacity", "0.5");
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#block2").click(function() {
+        $(this).toggleClass("active");
+
+        if ($(this).hasClass("active")) {
+            $(this).css("opacity", "1");
+        } else {
+            $(this).css("opacity", "0.5");
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#block3").click(function() {
+        $(this).toggleClass("active");
+
+        if ($(this).hasClass("active")) {
+            $(this).css("opacity", "1");
+        } else {
+            $(this).css("opacity", "0.5");
+        }
+    });
+});
+
+
+$(document).ready(function() {
+    $("#block4").click(function() {
+        $(this).toggleClass("active");
+
+        if ($(this).hasClass("active")) {
+            $(this).css("opacity", "1");
+        } else {
+            $(this).css("opacity", "0.5");
+        }
+    });
+});
+
+
+$(document).ready(function() {
+    $("#block5").click(function() {
+        $(this).toggleClass("active");
+
+        if ($(this).hasClass("active")) {
+            $(this).css("opacity", "1");
+        } else {
+            $(this).css("opacity", "0.5");
+        }
+    });
+});
+
+
+$(document).ready(function() {
+    $("#block6").click(function() {
+        $(this).toggleClass("active");
+
+        if ($(this).hasClass("active")) {
+            $(this).css("opacity", "1");
+        } else {
+            $(this).css("opacity", "0.5");
+        }
+    });
+});
+
+
+$(document).ready(function() {
+    $("#block7").click(function() {
+        $(this).toggleClass("active");
+
+        if ($(this).hasClass("active")) {
+            $(this).css("opacity", "1");
+        } else {
+            $(this).css("opacity", "0.5");
+        }
+    });
+});
+
+
+$(document).ready(function() {
+    $("#block8").click(function() {
+        $(this).toggleClass("active");
+
+        if ($(this).hasClass("active")) {
+            $(this).css("opacity", "1");
+        } else {
+            $(this).css("opacity", "0.5");
+        }
+    });
+});
+
+
+$(document).ready(function() {
+    $("#block9").click(function() {
+        $(this).toggleClass("active");
+
+        if ($(this).hasClass("active")) {
+            $(this).css("opacity", "1");
+        } else {
+            $(this).css("opacity", "0.5");
+        }
+    });
+});
